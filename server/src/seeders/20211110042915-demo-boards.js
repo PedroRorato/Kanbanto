e65971aea5902ae9beb3f653c88a336a62113bab
PedroRorato <pedroberorato@gmail.com>
2021-11-10 01:39:51 -0300

    create Board model

---
+++ server/src/seeders/20211110042915-demo-boards.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Boards', [{
+      name: 'Fazenda',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Boards', null, {});
+  }
+};

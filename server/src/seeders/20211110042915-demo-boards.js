98c169c7160d55b8e540354234f04ae8c68f2db9
PedroRorato <pedroberorato@gmail.com>
2021-11-11 20:40:38 -0300

    Board Seed

--- server/src/seeders/20211110042915-demo-boards.js
+++ server/src/seeders/20211110042915-demo-boards.js
@@ -2,11 +2,18 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Boards', [{
-      name: 'Fazenda',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    await queryInterface.bulkInsert('Boards', [
+      {
+        name: 'Fazenda',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Programação',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {

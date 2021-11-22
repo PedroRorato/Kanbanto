1b6185f491fc7a8d0a335d5aacb41ecae10f65a0
PedroRorato <pedroberorato@gmail.com>
2021-11-22 19:39:07 -0300

    create Input component

---
+++ client/src/components/Input/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+function Input(props) {
+  return (
+    <Container {...props} />
+  );
+}
+
+export default Input;
---
+++ client/src/components/Input/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.input`
+  border: 1px solid #ccc;
+  border-radius: .5rem;
+  font-size: 16px;
+  padding: .75rem;
+`;

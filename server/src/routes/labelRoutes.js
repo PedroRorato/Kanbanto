08296894e7a8676fc5adbf00956ec82bf25c1ed2
PedroRorato <pedroberorato@gmail.com>
2021-11-23 19:55:58 -0300

    add middleware to labelRoutes

--- server/src/routes/labelRoutes.js
+++ server/src/routes/labelRoutes.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
 
+//Middleware
+const verifyAuthentication = require("../middlewares/verifyAuthentication");
+
 //Controllers
 const LabelController = require("../controllers/LabelController");
 
@@ -7,11 +10,11 @@ const LabelController = require("../controllers/LabelController");
 const router = Router();
 
 //Routes
-router.get("/boards/:boardId/labels", LabelController.index);
-router.post("/boards/:boardId/labels", LabelController.create);
-router.get("/labels/:id", LabelController.show);
-router.put("/labels/:id", LabelController.update);
-router.delete("/labels/:id", LabelController.delete);
+router.get("/boards/:boardId/labels", verifyAuthentication, LabelController.index);
+router.post("/boards/:boardId/labels", verifyAuthentication, LabelController.create);
+router.get("/labels/:id", verifyAuthentication, LabelController.show);
+router.put("/labels/:id", verifyAuthentication, LabelController.update);
+router.delete("/labels/:id", verifyAuthentication, LabelController.delete);
 
 //Export
 module.exports = router;

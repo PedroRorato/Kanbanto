e1717e2cda8b8f4c9e4387e602bcf9a549183a65
PedroRorato <pedroberorato@gmail.com>
2021-11-23 22:08:39 -0300

    add unlogged links

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //Styles
 import { Container } from "./styles";
@@ -8,6 +9,11 @@ const Header = () => {
   return (
     <Container>
       <h1>Kanbanto</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+      </ul>
     </Container>
   );
 };
--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -3,5 +3,29 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: #fff;
   border-bottom:1px solid #ccc;
-  padding: 0 1rem;
+  display: flex;
+  padding: .5rem 1rem;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    display: flex;
+  }
+
+  ul a {
+    color: #333;
+    font-size: 16px;
+    padding: .75rem 0 .75rem 1rem;
+  }
+
+  ul a:hover {
+    opacity: .7;
+    transition: .3s;
+  }
+
+
 `;

cc094f29909cab70107da2d2756cfeaa9c8070d4
PedroRorato <pedroberorato@gmail.com>
2021-11-23 22:56:20 -0300

    create Card component

---
+++ client/src/components/Card/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: .5rem;
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  padding: 1rem;
+
+  header {
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    justify-content: center;
+    padding-bottom: .75rem;
+  }
+`;

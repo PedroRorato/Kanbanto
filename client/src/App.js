fee12ad64011b2ea8a94888886ad371b931a5661
PedroRorato <pedroberorato@gmail.com>
2021-11-24 00:05:50 -0300

    add AuthProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+//Context
+import { AuthProvider } from "./contexts/AuthProvider";
+
 //Pages
 import HomeScreen from "./pages/HomeScreen";
+import LoginScreen from "./pages/LoginScreen";
+import RegisterScreen from "./pages/RegisterScreen";
 import BoardsScreen from "./pages/BoardsScreen";
 import BoardScreen from "./pages/BoardScreen";
 
@@ -14,16 +19,18 @@ import GlobalStyle from "./assets/styles/global";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <GlobalStyle />
       <Layout>
         <Switch>
           <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
           <Route exact path="/boards" component={BoardsScreen} />
           <Route exact path="/boards/:id" component={BoardScreen} />
         </Switch>
       </Layout>
-    </>
+    </AuthProvider>
   );
 }
 

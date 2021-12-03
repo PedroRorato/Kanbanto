f7f8250a30db880a967a95b7cb0864c788632bc5
PedroRorato <pedroberorato@gmail.com>
2021-12-02 23:14:46 -0300

    add responsive behavior to header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,33 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 //Context
 import { useAuth } from "../../contexts/AuthProvider";
 
 //Styles
-import { Container } from "./styles";
+import { Container, List, MenuButton } from "./styles";
 
 //Main Function
 const Header = () => {
+  const { innerWidth: width } = window;
+
+  //Context
   const { user, logout } = useAuth();
+
+  //State
+  const [showMenu, setShowMenu] = useState(true);
+
+
+  useEffect(() => {
+    if (width < 576) {
+      setShowMenu(false);
+    }
+  }, []);
+
+  console.log(showMenu);
 
   return (
     <Container>
       <h1>Kanbanto</h1>
       {user ?
-        <>
-          <ul>
-            <li><Link to="/boards">Boards</Link></li>
-            <li><Link to="/settings">Settings</Link></li>
-            <button onClick={logout}>Logout</button>
-          </ul>
-        </> :
-        <ul>
+        <List show={showMenu}>
+          <li><Link to="/boards">Boards</Link></li>
+          <li><Link to="/settings">Settings</Link></li>
+          <button onClick={logout}>Logout</button>
+        </List> :
+        <List show={showMenu}>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/register">Register</Link></li>
-        </ul>
+        </List>
       }
+      <MenuButton onClick={() => setShowMenu(prev => !prev)}><FaBars size={20} /></MenuButton>
     </Container>
   );
 };
--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -7,28 +7,25 @@ export const Container = styled.div`
   padding: .5rem 1rem;
   align-items: center;
   justify-content: space-between;
+`;
+
+export const List = styled.ul`
+  display: ${props => props.show ? "flex" : "none"};
+  align-items: center;
 
   a {
-    text-decoration: none;
-  }
-
-  ul {
-    display: flex;
-    align-items: center;
-  }
-
-  ul a {
     color: #333;
     font-size: 16px;
     padding: .75rem 0 .75rem 1rem;
+    text-decoration: none;
   }
 
-  ul a:hover {
+  a:hover {
     opacity: .7;
     transition: .3s;
   }
 
-  ul button {
+  button {
     background-color: #222;
     border-radius: .25rem;
     color: #fff;
@@ -36,7 +33,59 @@ export const Container = styled.div`
     padding: .5rem .75rem;
   }
 
-  ul button:hover {
+  button:hover {
     opacity: .7;
   }
+
+  @media only screen and (max-width: 575px) {
+    background-color: #fff;
+    border-bottom: 1px solid #ccc;
+    flex-direction: column;
+
+    padding-bottom: .5rem;
+    position: absolute;
+    left: 0;
+    top: 54px;
+    width: 100vw;
+    z-index: 100;
+
+    li {
+      border-bottom: 1px solid #ccc;
+      height: 50px;
+      padding: 0;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
+
+    li a {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
+
+    button {
+      margin-top: .5rem
+    }
+  }
+`;
+
+export const MenuButton = styled.button`
+  background-color: #222;
+  border-radius: .25rem;
+  color: #fff;
+  display: none;
+  align-items: center;
+  justify-content: center;
+  height: 32px;
+  width: 32px;
+
+  &:hover {
+    opacity: .7;
+  }
+
+  @media only screen and (max-width: 576px) {
+    display: flex;
+  }
+
 `;

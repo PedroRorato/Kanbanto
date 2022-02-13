ff9afcfe831ba302cf62a1bdd98aec04f52e2f7a
PedroRorato <pedroberorato@gmail.com>
2022-02-12 22:24:37 -0300

    feat: add HomePage picture

--- client/src/pages/HomeScreen/index.js
+++ client/src/pages/HomeScreen/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-import { Container } from "./styles";
+// Assets
+import exemplo from "../../assets/img/exemplo.png";
+
+// Styles
+import { Container, ImageContainer } from "./styles";
 
 function HomeScreen() {
-  return (
-    <Container>
-      <h1>Organize seu projeto com Kanbanto</h1>
-    </Container>
-  );
+	return (
+		<Container>
+			<h1>Organize seu projeto com Kanbanto</h1>
+			<ImageContainer>
+				<img src={exemplo} />
+			</ImageContainer>
+		</Container>
+	);
 }
 
 export default HomeScreen;
--- client/src/pages/HomeScreen/styles.js
+++ client/src/pages/HomeScreen/styles.js
@@ -2,8 +2,27 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding: 1rem;
+	flex-direction: column;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
   h1 {
     text-align: center;
+  }
+`;
+
+export const ImageContainer = styled.div`
+	margin-top: 2rem;
+  padding: .5rem;
+	background-color: #555;
+	border: 3px solid white;
+	border-radius: .5rem;
+	max-width: 1440px;
+
+
+  img {
+		border-radius: .25rem;
+    width: 100%;
   }
 `;

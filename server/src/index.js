4ba96b366893db93c956649b507bd93a6826ef62
PedroRorato <pedroberorato@gmail.com>
2022-02-17 22:52:37 -0300

    Security improvements

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 
 //Custom files
 const routes = require("./routes");
 
 //Start app
 const app = express();
+app.disable("x-powered-by");
+
+//Hide version
+const corsOption = {
+  origin: "http://localhost:3000",
+};
 
 //Enable JSON
-app.use(cors());
+app.use(cors(corsOption));
 app.use(express.json());
+
+//Hide version
+app.use(helmet.hidePoweredBy());
 
 //Routes
 routes(app);
